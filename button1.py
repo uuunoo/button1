cd9666299406f1d3e6e6c94b6904529d22706520
Yunho Bae <uuuno130104@naver.com>
2024-09-23 11:20:31 +0900

    frist

--- button1.py
+++ button1.py
@@ -3,7 +3,7 @@ from tkinter import *
 window =Tk()
 
 b1 = Button(window, text="Yeon Ryu")
-b2 = Button(window, text="60221010")
+b2 = Button(window, text="60232016")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
